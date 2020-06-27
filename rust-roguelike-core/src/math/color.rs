9fc8f571acebef5c8d1ce4162e189da04062de20
Orchaldir <orchaldir@gmail.com>
2020-06-27 19:34:41 +0200

    [#1] Add Color

---
+++ rust-roguelike-core/src/math/color.rs
@@ -0,0 +1,58 @@
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct Color {
+    r: f32,
+    g: f32,
+    b: f32,
+}
+
+impl Color {
+    pub fn rgb(r: f32, g: f32, b: f32) -> Color {
+        Color { r, g, b }
+    }
+}
+
+pub const BLACK: Color = Color {
+    r: 0.0,
+    g: 0.0,
+    b: 0.0,
+};
+pub const BLUE: Color = Color {
+    r: 0.0,
+    g: 0.0,
+    b: 1.0,
+};
+pub const CYAN: Color = Color {
+    r: 0.0,
+    g: 1.0,
+    b: 1.0,
+};
+pub const GREEN: Color = Color {
+    r: 0.0,
+    g: 1.0,
+    b: 0.0,
+};
+pub const MAGENTA: Color = Color {
+    r: 1.0,
+    g: 0.0,
+    b: 1.0,
+};
+pub const RED: Color = Color {
+    r: 1.0,
+    g: 0.0,
+    b: 0.0,
+};
+pub const PINK: Color = Color {
+    r: 1.0,
+    g: 0.08,
+    b: 0.58,
+};
+pub const WHITE: Color = Color {
+    r: 1.0,
+    g: 1.0,
+    b: 1.0,
+};
+pub const YELLOW: Color = Color {
+    r: 1.0,
+    g: 1.0,
+    b: 0.0,
+};
