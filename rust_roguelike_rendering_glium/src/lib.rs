e5e4919afb55638faacaae272190a077cb794748
Orchaldir <orchaldir@gmail.com>
2020-06-28 12:13:53 +0200

    [#1] Add struct ColoredVertex

--- rust_roguelike_rendering_glium/src/lib.rs
+++ rust_roguelike_rendering_glium/src/lib.rs
@@ -1,4 +1,10 @@
+#[macro_use]
 extern crate glium;
 extern crate rust_roguelike_core;
 
 pub mod shader;
+pub mod vertex;
+
+use crate::vertex::ColoredVertex;
+
+implement_vertex!(ColoredVertex, position, color);
---
+++ rust_roguelike_rendering_glium/src/vertex.rs
@@ -0,0 +1,5 @@
+#[derive(Copy, Clone)]
+pub struct ColoredVertex {
+    pub position: [f32; 2],
+    pub color: [f32; 3],
+}
