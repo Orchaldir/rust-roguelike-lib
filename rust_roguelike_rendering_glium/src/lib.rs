226f2196c96412b5bf97ee7ed73163b085eccd10
Orchaldir <orchaldir@gmail.com>
2020-06-29 22:20:54 +0200

    [#2] Add struct TexturedVertex

--- rust_roguelike_rendering_glium/src/lib.rs
+++ rust_roguelike_rendering_glium/src/lib.rs
@@ -8,5 +8,7 @@ pub mod vertex;
 pub mod window;
 
 use crate::vertex::ColoredVertex;
+use crate::vertex::TexturedVertex;
 
 implement_vertex!(ColoredVertex, position, color);
+implement_vertex!(TexturedVertex, position, color, tc);
--- rust_roguelike_rendering_glium/src/vertex.rs
+++ rust_roguelike_rendering_glium/src/vertex.rs
@@ -3,3 +3,10 @@ pub struct ColoredVertex {
     pub position: [f32; 2],
     pub color: [f32; 3],
 }
+
+#[derive(Copy, Clone)]
+pub struct TexturedVertex {
+    pub position: [f32; 2],
+    pub color: [f32; 3],
+    pub tc: [f32; 2], // texture coordinates
+}
