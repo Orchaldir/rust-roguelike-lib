de8e1a0f07cc1ce6e0227759f38bceb2b66bed67
Orchaldir <orchaldir@gmail.com>
2020-07-04 10:47:21 +0200

    [#7] Convert to the equivalent point

--- rust_roguelike_core/src/math/size2d.rs
+++ rust_roguelike_core/src/math/size2d.rs
@@ -1,5 +1,6 @@
 use std::ops::{Add, Mul};
 
+/// Defines the size of something in 2 dimensions.
 #[derive(PartialEq, Copy, Clone, Debug)]
 pub struct Size2d {
     x: u32,
@@ -9,16 +10,64 @@ pub struct Size2d {
 pub const ZERO: Size2d = Size2d { x: 0, y: 0 };
 
 impl Size2d {
+    /// Create a new Size2d
     pub fn new(x: u32, y: u32) -> Size2d {
         Size2d { x, y }
     }
 
+    /// Get the size along the x-axis
+    ///
+    /// ```
+    ///# use rust_roguelike_core::math::size2d::Size2d;
+    /// let size = Size2d::new(2, 3);
+    /// assert_eq!(size.x(), 2);
+    /// ```
     pub fn x(&self) -> u32 {
         self.x
     }
 
+    /// Get the size along the y-axis
+    ///
+    /// ```
+    ///# use rust_roguelike_core::math::size2d::Size2d;
+    /// let size = Size2d::new(2, 3);
+    /// assert_eq!(size.y(), 3);
+    /// ```
     pub fn y(&self) -> u32 {
         self.y
+    }
+
+    /// Convert an index to the x-coordinate of the equivalent point
+    ///
+    /// ```
+    ///# use rust_roguelike_core::math::size2d::Size2d;
+    /// let size = Size2d::new(2, 3);
+    /// assert_eq!(size.to_x(5), 1);
+    /// ```
+    pub fn to_x(&self, index: usize) -> u32 {
+        index as u32 % self.x
+    }
+
+    /// Convert an index to the y-coordinate of the equivalent point
+    ///
+    /// ```
+    ///# use rust_roguelike_core::math::size2d::Size2d;
+    /// let size = Size2d::new(2, 3);
+    /// assert_eq!(size.to_y(5), 2);
+    /// ```
+    pub fn to_y(&self, index: usize) -> u32 {
+        index as u32 / self.x
+    }
+
+    /// Convert an index to the equivalent point
+    ///
+    /// ```
+    ///# use rust_roguelike_core::math::size2d::Size2d;
+    /// let size = Size2d::new(2, 3);
+    /// assert_eq!(size.to_position(5), [1,2]);
+    /// ```
+    pub fn to_point(&self, index: usize) -> [u32; 2] {
+        [self.to_x(index), self.to_y(index)]
     }
 }
 
