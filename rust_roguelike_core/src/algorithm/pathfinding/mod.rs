6480dd28a31727efee5e513f2e90cfb05dd6b42b
Orchaldir <orchaldir@gmail.com>
2020-07-12 11:25:04 +0200

    [#9] Add CostCalculator, PathfindingAlgorithm & PathfindingResult

---
+++ rust_roguelike_core/src/algorithm/pathfinding/mod.rs
@@ -0,0 +1,23 @@
+use crate::math::graph::{Graph, Neighbor};
+
+pub enum PathfindingResult<E> {
+    GoalAlreadyReached,
+    NotSearched,
+    NoPathFound,
+    Path {
+        total_cost: u32,
+        neighbors: Vec<Box<Neighbor<E>>>,
+    },
+}
+
+pub trait CostCalculator<E> {
+    fn is_valid(&self, index: usize) -> bool;
+
+    fn calculate_cost(&self, index: usize, neighbor: &Neighbor<E>) -> u32;
+}
+
+pub trait PathfindingAlgorithm<N, E> {
+    fn find<G>(&self, graph: &G, start: usize, goal: usize) -> PathfindingResult<E>
+    where
+        G: Graph<N, E> + CostCalculator<E>;
+}
--- rust_roguelike_core/src/lib.rs
+++ rust_roguelike_core/src/lib.rs
@@ -1,3 +1,4 @@
+pub mod algorithm;
 pub mod interface;
 pub mod math;
 pub mod rendering;
